681db0409a06a191a1a5e511987c05ce4649fa47
hey_hongh_glb <hey_hongh_glb@163.com>
2018-12-21 16:07:00 +0800

    edit by hot_fix...

--- src/main/java/com/zhiyou/Apple.java
+++ src/main/java/com/zhiyou/Apple.java
@@ -5,5 +5,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("TEst5...");
 		System.out.println("TEst6...");
+		System.out.println("test by 2018/12/21");
 	}
 }
